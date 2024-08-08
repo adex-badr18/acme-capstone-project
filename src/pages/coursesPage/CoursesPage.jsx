4e82e2fba012662ea2635aa1b219f2a0a56919c0
Megagig <megagigsoftwaresolutions.com>
2024-08-08 16:17:34 +0100

    Implement the CoursesSection

--- src/pages/coursesPage/CoursesPage.jsx
+++ src/pages/coursesPage/CoursesPage.jsx
@@ -1,8 +1,10 @@
 import HeroSection from './components/HeroSection';
+import CourseSection from './components/CourseSection';
 const CoursesPage = () => {
   return (
     <div>
       <HeroSection />
+      <CourseSection />
     </div>
   );
 };
