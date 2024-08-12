f4a33bc946ab5f7510f0602ea1f28eb9e19e3ab5
daniel_eln <omoniyiopemipo6@gmail.com>
2024-08-12 11:49:17 +0100

    [UI fix] fixed issue of images following each other

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -22,6 +22,12 @@ const AboutUs = () => {
         <div id="text" className='flex flex-col gap-4  lg:py-6 lg:gap-8'>
           <h1 className='text-5xl text-[#1B40B3] font-extrabold lg:text-6xl'>About Us</h1>
           <h2 className='text-xl font-bold lg:text-4xl'>Training And Consulting Company</h2>
+          
+          <div id="images-herosection" className='bg-[#F2F2F2] py-8 rounded-lg self-center relative sm:max-w-[450px] lg:hidden'>
+            <img src="/about-herosection-1.png" alt="about-herosection-1" className='relative -right-6 -top-4 md:-right-24' />
+            <img src="/about-herosection-2.png" alt="about-herosection-1" className='relative -left-6 -bottom-4 md:-left-24'/>
+          </div>
+          
           <p className='text-sm leading-loose lg:text-lg lg:leading-[2.7rem]'>
             At ACME Software Lab, we are passionate about empowering individuals and organizations through innovative training and consulting
             services. As a leading provider in the tech industry, our mission is to bridge the gap between technology and talent, helping our 
@@ -29,7 +35,7 @@ const AboutUs = () => {
           </p>
         </div>
 
-        <div id="images-herosection" className='bg-[#F2F2F2] rounded-lg self-center relative sm:max-w-[450px] lg:min-w-[450px]'>
+        <div id="images-herosection" className='bg-[#F2F2F2] hidden rounded-lg self-center relative sm:max-w-[450px] lg:min-w-[450px] lg:block'>
           <img src="/about-herosection-1.png" alt="about-herosection-1" className='relative -right-6 -top-4 md:-right-24' />
           <img src="/about-herosection-2.png" alt="about-herosection-1" className='relative -left-6 -bottom-4 md:-left-24'/>
         </div>
@@ -43,13 +49,20 @@ const AboutUs = () => {
       */}
 
       <div id="mission" className='flex px-8 flex-col-reverse gap-12 py-8 xs:flex-row xl:flex-row lg:px-36 xl:gap-20'>
-        <div id="images-mission" className='bg-gray-100 relative rounded-lg px-2 sm:max-w-[450px] sm:self-center lg:min-w-[450px] xl:self-start'>
+        <div id="images-mission" className='hidden bg-gray-100 relative rounded-lg px-2 sm:max-w-[450px] sm:self-center lg:block lg:min-w-[450px] xl:self-start'>
           <img src="/mission-about.png" alt="mission-section-img" className='relative top-5'/>
         </div>
 
         <div id='text' className='flex flex-col gap-4'>
           <div id="mission">
             <h2 className='text-2xl font-bold text-[#1B40B3]'>Our Mission</h2>
+
+            <div className='w-full py-8 flex justify-center items-center self-center lg:hidden'>
+              <div id="images-mission" className='bg-gray-100 relative rounded-lg px-2 sm:max-w-[450px] sm:self-center lg:min-w-[450px]'>
+                <img src="/mission-about.png" alt="mission-section-img" className='relative top-5'/>
+              </div>
+            </div>
+
             <p className='text-sm py-4 leading-loose lg:text-base'>
               At ACME Software Lab, our mission is simple yet profound: to cultivate a community of tech-savvy professionals 
               equipped with the expertise needed to thrive in today's digital landscape. We strive to bridge the gap between 
